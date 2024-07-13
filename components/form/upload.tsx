2356eead1a25fe0be7f8241acb00b61b089bb6df
osaulenkoUa <oom2212@gmail.com>
2024-07-13 13:34:34 +0300

    added resize

--- components/form/upload.tsx
+++ components/form/upload.tsx
@@ -2,6 +2,7 @@
 
 import React, {ChangeEvent, FormEvent, useState} from 'react';
 import axios from "axios";
+import Resizer from "react-image-file-resizer";
 
 interface UploadFormProps {
     fileName:string
@@ -9,8 +10,24 @@ interface UploadFormProps {
 
 export const UploadForm: React.FC<UploadFormProps> = ({fileName}) => {
 
-    const [file, setFile] = useState<File | null>(null);
+    const [file, setFile] = useState<any>(null);
     const [targetDir, setTargetDir] = useState<string>('/home/alex/web/himdecor/public/images/shop/');
+
+    const resizeFile = (file: Blob) =>
+        new Promise((resolve) => {
+            Resizer.imageFileResizer(
+                file,
+                300,
+                300,
+                "JPEG",
+                80,
+                0,
+                (uri) => {
+                    resolve(uri);
+                },
+                "file"
+            );
+        });
 
     const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
         if (e.target.files && e.target.files[0]) {
@@ -24,17 +41,17 @@ export const UploadForm: React.FC<UploadFormProps> = ({fileName}) => {
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         if (!file || !targetDir) return;
-
-        const fileBuffer = await file.arrayBuffer();
+        const image = await resizeFile(file) as Blob;
+        const fileBuffer = await image.arrayBuffer();
         const decoder = new TextDecoder();
         const str = decoder.decode(fileBuffer);
         const requestBody = {
             file: str,
-            fileName,
+            fileName: file.name,
             targetDir,
         };
         try {
-            const response = await axios.post('/api/image', requestBody, {
+            const response = await axios.post('https://himdecor-back-new.vercel.app/file/uploadFile', requestBody, {
                 headers: {
                     'Content-Type': 'application/json',
                 },
@@ -62,6 +79,7 @@ export const UploadForm: React.FC<UploadFormProps> = ({fileName}) => {
                 </div>
                 <button type="submit">Upload</button>
             </form>
+            {file && <img src={URL.createObjectURL(file)} alt="Resized" />}
         </div>
     );
 };
--- src/app/dashboard/addProduct/page.tsx
+++ src/app/dashboard/addProduct/page.tsx
@@ -1,84 +1,19 @@
 "use client";
 
-import React, {ChangeEvent, FormEvent, useState} from 'react';
-import axios from "axios";
+import React from 'react';
+import {UploadForm} from "@/components/form/upload";
 
 interface UploadFormProps {
 }
 
 
-const UploadForm: React.FC<UploadFormProps> = () => {
+const AddProduct: React.FC<UploadFormProps> = () => {
 
-    const [file, setFile] = useState<File | null>(null);
-    const [targetDir, setTargetDir] = useState<string>('/home/alex/web/');
-
-    const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files && e.target.files[0]) {
-            setFile(e.target.files[0]);
-        }
-    };
-    const handleTargetDirChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setTargetDir(e.target.value);
-    };
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-        if (!file || !targetDir) return;
-
-        const fileBuffer = await file.arrayBuffer();
-        const decoder = new TextDecoder();
-        const str = decoder.decode(fileBuffer);
-        const requestBody = {
-            file: str,
-            fileName: file.name,
-            targetDir,
-        };
-        try {
-            const response = await axios.post('https://himdecor-back-new.vercel.app/file/uploadFile', requestBody, {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            });
-            console.log(response)
-        } catch (error) {
-            console.error('Error uploading file:', error);
-        }
-    };
-
-
-    const onHandleShowList = async ()=>{
-        try{
-           const list=  await axios.get('/api/getList')
-            console.log(list)
-        }
-        catch (err){
-            console.log(err)
-        }
-    }
-
-
-    return (
+     return (
         <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>
-                        Select File:
-                        <input type="file" onChange={handleFileChange}/>
-                    </label>
-                </div>
-                <div>
-                    <label>
-                        Target Directory:
-                        <input type="text" value={targetDir} onChange={handleTargetDirChange}/>
-                    </label>
-                </div>
-                <button type="submit">Upload</button>
-            </form>
-
-
-            <button className={'border-gray-500 border m-8 cursor-pointer p-2'} type="button" onClick={onHandleShowList}>Show files list</button>
-
+           <UploadForm fileName={'wqe'}/>
         </div>
     );
 };
 
-export default UploadForm;
+export default AddProduct;
